7d0c732ff63d8661894745baf6ac63b2a86d8e5f
tigor <tigor@Redragon.lan>
2023-10-03 17:41:44 -0300

    First commit

---
+++ DEV-Fragances/src/main/java/com/mycompany/projetoperfumaria/ProjetoPerfumaria.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoperfumaria;
+
+/**
+ *
+ * @author giancarlomarques
+ */
+public class ProjetoPerfumaria {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ DEV-Fragances/src/main/java/models/Produto.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package models;
+
+/**
+ *
+ * @author giancarlomarques
+ */
+public class Produto {
+    
+     private int id;
+    private String nome;
+    private double preco;
+    private int qtd;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQtd() {
+        return qtd;
+    }
+
+    public void setQtd(int qtd) {
+        this.qtd = qtd;
+    }
+   
+    
+    public Produto(String nome, double preco, int qtd){
+        this.nome = nome;
+        this.preco = preco;
+        this.qtd = qtd;
+    }
+    
+    
+}
